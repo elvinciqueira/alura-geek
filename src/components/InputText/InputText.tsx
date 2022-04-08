9665852ea2cd4b27551b5801209f2de12f81e1ef
elvinciqueira <elvinciqueira@gmail.com>
2022-04-08 15:01:59 -0300

    feat: InputText component

---
+++ src/components/InputText/InputText.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components';
+import { InputBase, InputProps } from '../InputBase';
+import { TextStyleVariants } from '../Text';
+
+export type InputTextProps = {
+  label?: string;
+  className?: string;
+} & InputProps;
+
+export const InputText = ({
+  name,
+  label,
+  className,
+  ...props
+}: InputTextProps) => (
+  <Container className={className}>
+    <Label htmlFor={name}>{label}</Label>
+    <InputTextBase name={name} aria-label={label} {...props} />
+  </Container>
+);
+
+const Container = styled.div`
+  border-radius: ${({ theme }) => theme.borderRadius.borderRadiusXs};
+  border-bottom: 1px solid #c8c8c8;
+  display: flex;
+  flex-direction: column;
+  padding: ${({ theme }) => theme.space.spacingInsetSizeNanoStack};
+`;
+
+const Label = styled.label`
+  color: ${({ theme }) => theme.colors.neutralGray500};
+  font-family: ${({ theme }) => theme.fonts.fontFamily};
+  font-size: ${({ theme }) => theme.fontSizes.fontSizeXxxs};
+  font-weight: ${({ theme }) => theme.fontWeights.fontWeightRegular};
+  line-height: 16px;
+  margin-bottom: ${({ theme }) => theme.space.spacingSizeQuarck};
+`;
+
+const InputTextBase = styled(InputBase)`
+  ${TextStyleVariants['textBodyRegular']};
+  line-height: 20px;
+`;
